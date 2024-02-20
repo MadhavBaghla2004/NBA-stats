f1dfdcfdd54ac79d675da5dc92c1bc09f7aec75f
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-19 22:16:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,9 @@ df_team = df[df['team'] == team].reset_index(drop=True)
 df_team['players_list'] = df_team['players_list'].str.replace(r"[\"\' \[\]]", '').str.split(',')
 duplicate_roster = df_team['players_list'].apply(pd.Series).stack()
 roster = duplicate_roster.unique()
-for player in roster:
-  player.strip()
+roster = [player.strip('[]') for player in roster]
+
+
 
 
 
