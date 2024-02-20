f1269ad80ad2764b28ecb0936860b43478b0240e
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-19 22:45:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ df_team = df[df['team'] == team].reset_index(drop=True)
 df_team['players_list'] = df_team['players_list'].str.replace(r"[\"\' \[\]]", '').str.split(',')
 duplicate_roster = df_team['players_list'].apply(pd.Series).stack()
 roster = duplicate_roster.unique()
-st.write(type(roster))
+new_roster=roster.tolist()
+st.write(new_roster)
 
 
 players = st.multiselect(
