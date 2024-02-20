02671c97670f3d24a01f098a9fb805a072fe076e
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-19 22:32:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import base64
-import numpy as np
+
 
 # import data
 df = pd.read_csv('NBAlineup.csv')
@@ -25,7 +25,7 @@ df_team = df[df['team'] == team].reset_index(drop=True)
 df_team['players_list'] = df_team['players_list'].str.replace(r"[\"\' \[\]]", '').str.split(',')
 duplicate_roster = df_team['players_list'].apply(pd.Series).stack()
 roster = duplicate_roster.unique()
-roster_stripped = np.char.strip(roster, '[]')
+
 
 
     
