7e639f92eade1cd6374f53314f5671a13f2c554d
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-20 11:51:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -144,9 +144,12 @@ if len(players) == 5:
      
     else:
         possible_lineup = df_team.loc[0, 'players_list_stripped']
-        st.warning("This group of players did not play together this season, hence there is no data available.") 
-        st.warning("Please select a different group")
-        st.warning(f"Possible lineup: {possible_lineup}")
+        warning_message = (
+            "This group of players did not play together this season, hence there is no data available. \n"
+            "Please select a different group. \n"
+            f"Possible lineup: {possible_lineup}"
+        )
+        st.warning(warning_message)
 else:
     st.warning("Please select exactly 5 players for the lineup.")
 
