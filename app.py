11c10004657f789e602aa2bef357361706b6d19f
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-19 22:36:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import base64
+import numpy as np
 
 
 # import data
@@ -25,6 +26,9 @@ df_team = df[df['team'] == team].reset_index(drop=True)
 df_team['players_list'] = df_team['players_list'].str.replace(r"[\"\' \[\]]", '').str.split(',')
 duplicate_roster = df_team['players_list'].apply(pd.Series).stack()
 roster = duplicate_roster.unique()
+roster_stripped = np.char.strip(roster, '[]')
+
+st.write(roster_stripped)
 
 
 
