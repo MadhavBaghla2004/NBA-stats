392244f01346c4dd7f1bf595439f5fbc68b4a5d4
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-20 12:10:39 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,13 @@ df = pd.read_csv('NBAlineup.csv')
 # Title for app
 st.set_page_config(layout="wide")
 st.title('NBA Lineup Analysis Tool')
-st.write('Using this tool we can analyse some key statistics in the NBA for the 2021-22 season, picking our best starting lineup.')
-st.write('You can see the difference in 3 key metrics- namely plus minus, field goal percentage and 3 point percentage.')
-st.write('We can pick the best starting lineup using these three key metrics.')
+st.markdown("""
+```
+     Using this tool we can analyse some key statistics in the NBA for the 2021-22 season, picking our best starting lineup.
+     You can see the difference in 3 key metrics- namely plus minus, field goal percentage and 3 point percentage.
+     We can pick the best starting lineup using these three key metrics.
+```
+""",True)
 
 
 # User chooses team
