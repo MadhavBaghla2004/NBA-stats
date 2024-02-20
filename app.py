17943296d4320274eb52b8e8caf13ff54b1ca9fd
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-20 10:31:20 -0800

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ if len(players) == 5:
 
         with st.container():
             fig = px.scatter(df_team, x="MIN", y="GP", title="Scatter Plot of Minutes vs. Games Played For the Squad", 
-                             hover_data={'GROUP_NAME': True},text=[f"MINUTES: {min_value}<br>GAMES_PLAYED: {gp_value}<br>LINEUP: {group_name}"])
+                             hover_data={'GROUP_NAME': True})
             fig.update_xaxes(title_text="MINUTES")
             fig.update_yaxes(title_text="GAMES PLAYED")
             min_value = df_important['MIN'].values[0]
