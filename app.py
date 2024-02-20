3c6ce40a7423336a5c68dde2bf4f6b028d313fa0
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-20 01:40:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,10 @@ team = st.selectbox(
      df['team'].unique())
 
 
-df_team = df[df['team'] == team].reset_index(drop=True)
 df_team['players_list'] = df_team['players_list'].str.replace(r"[\"\' \[\]]", '').str.split(',')
 duplicate_roster = df_team['players_list'].apply(pd.Series).stack()
-uniq_names = set(duplicate_roster)
-roster = [player.replace('[', '').replace(']', '').strip("'").replace("'", "") for player in uniq_names]
+roster = duplicate_roster.unique()
+roster = [player.replace('[', '').replace(']', '').strip("'").replace("'", "") for player in roster]
 
 
 
