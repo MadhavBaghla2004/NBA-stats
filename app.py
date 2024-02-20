82411489fa39c283689d52e6d88f983534d7620b
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-20 05:03:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if len(players) == 5:
      
         df_important = df_lineup[['MIN', 'PLUS_MINUS','FG_PCT', 'FG3_PCT']]
         df_important.reset_index(drop=True)
-        df_important['Stat'] = 'Value'
+        df_important['STAT'] = 'VALUE'
         df_important.set_index('Stat', inplace=True)
         st.write(df_important)
 
