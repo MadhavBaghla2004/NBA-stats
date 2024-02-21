020d554901a530ff30010bfebd9ea1cd4605a636
Madhav Baghla <madhavbaghla2@gmail.com>
2024-02-20 20:23:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -187,8 +187,38 @@ def add_bg_from_local(image_file):
     )
 add_bg_from_local('bg.png')
 
-[ui]
-hideTopBar = true
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 
